9f9723f2308e49def71f81b4aeac3bab31547818
tuachout <vikrant.thakur@gmail.com>
2023-03-08 11:02:23 -0800

    get id end point

--- index.js
+++ index.js
@@ -3,10 +3,13 @@ const app = express();
 
 let pagals =  [ {id: 1, name:"Bhaloo"}];
 
+// show welcome banner
 app.get('/', (req,res)=> {
     res.send("Welcome to pagalon ka adda!\
               visit /help to know more");
 })
+
+// return general help
 app.get('/help', (req,res)=> {
     const help = "Use one of following end point \
                   help - for this screen \
@@ -14,13 +17,26 @@ app.get('/help', (req,res)=> {
     res.send(help);
 })
 
+// return help for api endpoint
 app.get('/api', (req,res)=> {
     const help = "Use one of following end point \
                   /api/pagal - for all adda member";
     res.send(help);
 })
 
+// return information about only one pagal
+app.get('/api/pagal/:id', (req,res)=> {
+    const idToLook = parseInt(req.params.id);
+    const pagal = pagals.find(p => p.id == idToLook);
+    if (pagal) {
+        return res.send(pagal)
+    } else {
+        return res.status(404).send('No pagal with this id found')
+    }
+    res.send(help);
+})
 
+// return information about all pagals
 app.get('/api/pagal/all', (req,res)=> {    
     res.send(pagals);
 })
